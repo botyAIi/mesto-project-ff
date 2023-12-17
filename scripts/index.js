6c04664ce4a1b89027f6fe41999fd027c0253c40
botyAIi <vadim.startzev@yandex.ru>
2023-12-17 12:24:24 +0300

    add "arial-labes" and fix JS

--- scripts/index.js
+++ scripts/index.js
@@ -4,29 +4,32 @@ const cardTemplate = document.querySelector('#card-template').content;
 const content = document.querySelector('.content');
 const listCards = content.querySelector('.places__list');
 // @todo: Функция создания карточки
-function addCard(Card) {
+function createCard(card, deleteCard) {
   const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
   const likeCardButton = cardElement.querySelector('.card__like-button');
-  const deleteButton = cardElement.querySelector('.card__delete-button');
 
   likeCardButton.addEventListener('click', (evt) => {
     evt.target.classList.toggle('card__like-button_is-active');
   });
 
-  cardElement.querySelector('.card__image').src = Card.link;
-  cardElement.querySelector('.card__image').alt = Card.name;
-  cardElement.querySelector('.card__title').textContent = Card.name;
+  cardElement.querySelector('.card__image').src = card.link;
+  cardElement.querySelector('.card__image').alt = card.name;
+  cardElement.querySelector('.card__title').textContent = card.name;
 
-  listCards.append(cardElement);
+  deleteCard(cardElement);
 
-  deleteButton.addEventListener('click', () => { 
-    deleteCard(cardElement)
-  });
+  return cardElement;
 }
 // @todo: Функция удаления карточки
-function deleteCard(element) {
-  element.remove();
+function deleteCard(targetCard) {
+  const deleteButton = targetCard.querySelector('.card__delete-button');
+
+  deleteButton.addEventListener('click', () => {
+    targetCard.remove();
+  });
 }
 // @todo: Вывести карточки на страницу
 
-initialCards.forEach(addCard);
+initialCards.forEach((card) => {
+  listCards.append(createCard(card, deleteCard));
+});
