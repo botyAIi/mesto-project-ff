ef01d995290f31860646f44895a0ead1340d6b73
botyAIi <vadim.startzev@yandex.ru>
2023-12-17 14:57:38 +0300

    fix deleteButton for review

--- scripts/index.js
+++ scripts/index.js
@@ -7,29 +7,24 @@ const listCards = content.querySelector('.places__list');
 function createCard(card, deleteCard) {
   const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
   const likeCardButton = cardElement.querySelector('.card__like-button');
-
+  const deleteButton = cardElement.querySelector('.card__delete-button');
+  
   likeCardButton.addEventListener('click', (evt) => {
     evt.target.classList.toggle('card__like-button_is-active');
   });
-
+  
   cardElement.querySelector('.card__image').src = card.link;
   cardElement.querySelector('.card__image').alt = card.name;
   cardElement.querySelector('.card__title').textContent = card.name;
-
-  deleteCard(cardElement);
-
+  
+  deleteButton.addEventListener('click', () => deleteCard(cardElement));
   return cardElement;
-}
+};
 // @todo: Функция удаления карточки
-function deleteCard(targetCard) {
-  const deleteButton = targetCard.querySelector('.card__delete-button');
-
-  deleteButton.addEventListener('click', () => {
-    targetCard.remove();
-  });
-}
+function deleteCard(card) {
+    card.remove();
+};
 // @todo: Вывести карточки на страницу
-
 initialCards.forEach((card) => {
   listCards.append(createCard(card, deleteCard));
 });
